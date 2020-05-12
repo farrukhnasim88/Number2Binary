26a4018e7a509d1d65c6d30e571a6863ce347ad3
FARRUKH Nasim <farrukh.nasim1@studytafensw.edu.au>
2020-05-12 22:50:15 +1000

    Add project files.

---
+++ Number2Binary/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Number2Binary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnBinary = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(60, 114);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(244, 26);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnBinary
+            // 
+            this.btnBinary.Location = new System.Drawing.Point(104, 176);
+            this.btnBinary.Name = "btnBinary";
+            this.btnBinary.Size = new System.Drawing.Size(132, 47);
+            this.btnBinary.TabIndex = 1;
+            this.btnBinary.Text = "Check Binary";
+            this.btnBinary.UseVisualStyleBackColor = true;
+            this.btnBinary.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please enter a number to see Binary";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBinary);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnBinary;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Number2Binary/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number2Binary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text) || string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                MessageBox.Show("Please enter a value");
+            }
+
+            else
+            {
+                int input = int.Parse(txtInput.Text);
+                int save = input;
+
+                if (input <= 0 )
+                {
+                    MessageBox.Show("Please enter whole number");
+                }
+
+                else
+                {
+                    int count = 0;
+                    string binary = "";
+
+                    while (input >= 1)
+                    {
+                        int div = input / 2;
+                        int rem = input % 2;
+                        if (rem == 1)
+                        {
+                            count++;
+                            string a = rem.ToString();
+                            binary += a;
+                        }
+                        else
+                        {
+
+                            binary += 0.ToString();
+                        }
+
+
+                        input = div;
+
+
+                    }
+
+                    char[] reverse = binary.ToCharArray();
+                    Array.Reverse(reverse);
+                    string n = "";
+                    n = new string(reverse);
+
+
+                    MessageBox.Show("Binary number of  " + save.ToString() + " => " + n + "\nToatl number of Ones in this binary are => " + count.ToString());
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
